ece491fed7e1f04753c410d405116d955ff2bc42
M4nvid <manvid.prigrano@gmail.com>
2026-05-16 08:46:27 +0300

    4: Made a user prompt

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
+import argparse
 from dotenv import load_dotenv
+
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -11,8 +13,11 @@ from google import genai
 client = genai.Client(api_key=api_key)
 
 def main():
+    parser = argparse.ArgumentParser(description="Parsing arguments for the agent")
+    parser.add_argument("user_prompt", type=str, help="Adds a User prompt")
+    args = parser.parse_args()
     response = client.models.generate_content(
-    model='gemini-2.5-flash', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    model='gemini-2.5-flash', contents=args.user_prompt
 )
     
     if response.usage_metadata is None:
