3dfeb40f6e3e56d8bfeda5c79f90d55dfddaefef
M4nvid <manvid.prigrano@gmail.com>
2026-05-17 07:21:29 +0300

    6: Added verbose argument

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ client = genai.Client(api_key=api_key)
 def main():
     parser = argparse.ArgumentParser(description="Parsing arguments for the agent")
     parser.add_argument("user_prompt", type=str, help="Adds a User prompt")
-
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
 
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
@@ -28,8 +28,10 @@ def main():
     if response.usage_metadata is None:
         raise RuntimeError ("Failed API request?")
     
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if args.verbose == True:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print("Response:")
     print(response.text)
 
