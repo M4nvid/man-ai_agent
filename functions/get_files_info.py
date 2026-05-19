8817b73eeb7f178eadbdc4009f3022bf1af1df6b
M4nvid <manvid.prigrano@gmail.com>
2026-05-19 08:01:42 +0300

    9: Completed the file_info function

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,10 +1,18 @@
 import os
 def get_files_info(working_directory, directory="."):
-     absolute_path = os.path.abspath(working_directory)
-     target_dir = os.path.normpath(os.path.join(absolute_path, directory))     
-     valid_target_dir = os.path.commonpath([absolute_path, target_dir]) == absolute_path
-     if valid_target_dir == False:
-          return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
-     if os.path.isdir(directory) == False:
-          return f'Error: "{directory}" is not a directory'
-     return f'Success: "{directory}" is within the working directory'
+     try:
+          absolute_path = os.path.abspath(working_directory)
+          target_dir = os.path.normpath(os.path.join(absolute_path, directory))     
+          valid_target_dir = os.path.commonpath([absolute_path, target_dir]) == absolute_path
+          if valid_target_dir == False:
+               return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+          if os.path.isdir(target_dir) == False:
+               return f'Error: "{target_dir}" is not a directory'
+          lines = []
+          for name in os.listdir(target_dir):
+               full_path = os.path.join(target_dir, name)
+               size = os.path.getsize(full_path)
+               lines.append( f'- {name}: file_size={size} bytes, is_dir={os.path.isdir(full_path)}' )
+          return "\n".join(lines)
+     except Exception as e:
+          return f"Error: {e}"
