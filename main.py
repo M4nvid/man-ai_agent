cea97823da73b0d8875e7bf633d4cf61c2f6b106
M4nvid <manvid.prigrano@gmail.com>
2026-05-27 07:41:15 +0300

    16: Implemented proper function calling

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ if api_key is None:
 
 from google import genai
 from google.genai import types
-from call_function import available_functions
+from call_function import available_functions, call_function
 
 client = genai.Client(api_key=api_key)
 
@@ -27,6 +27,7 @@ def main():
     tools=[available_functions], system_instruction=system_prompt
 )
 )
+    function_results = []
     
     if response.usage_metadata is None:
         raise RuntimeError ("Failed API request?")
@@ -35,13 +36,23 @@ def main():
         print(f"User prompt: {args.user_prompt}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        
        
     if not response.function_calls:
         print("Response:")
         print(response.text)
     else:
         for function_call in response.function_calls: 
-            print (f"Calling function: {function_call.name}({function_call.args})")
+            function_call_result = call_function(function_call, args.verbose)
+            if not function_call_result.parts:
+                raise Exception ("Error: .parts is empty")
+            if function_call_result.parts[0].function_response is None:
+                raise Exception ("Error: No function response")
+            if function_call_result.parts[0].function_response.response is None:
+                raise Exception ("Error: No response from the function response")
+            if args.verbose:
+                print(f"-> {function_call_result.parts[0].function_response.response}")
+            function_results.append(function_call_result.parts[0])
 
 
 if __name__ == "__main__":
